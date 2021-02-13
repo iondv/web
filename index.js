36cb436db24a96a2ba0c27adec50acefa1b34a16
Dan Krasilnikov <krasilneg@yandex.ru>
2021-02-13 14:14:56 +1000

    fix

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  SessionHandler: require('./lib/session'),
+  oAuthAdapter: require('./lib/oAuthAdapter'),
+  wsAuth: require('./lib/wsAuth'),
+  backend: require('./lib/backend'),
+  util: require('./lib/util'),
+  server: require('./lib/server'),
+};
